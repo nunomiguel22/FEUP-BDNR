67e90b5a38bd7a48d86adc6f2e46d66aead61f50
nunomiguel22 <nunomiguel22@gmail.com>
2022-05-05 17:39:53 +0100

    updated query

--- project/query_1.py
+++ project/query_1.py
@@ -1,9 +1,30 @@
 # pylint: skip-file
 from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+import pprint
 
+MOVIE_NAME = "Justice League"
+
+pp = pprint.PrettyPrinter(indent=4)
 g = traversal().with_remote(DriverRemoteConnection(
     'ws://127.0.0.1:8182/gremlin', 'g'))
 
+print('\nMOVIE INFO\n\n')
+movie = g.V().hasLabel('movies').has('original_title', MOVIE_NAME).next()
+movie_info = g.V(movie.id).valueMap().next()
+pp.pprint(movie_info)
 
-movies = g.V().hasLabel('john').next()
+print('\nACTOR NAMES\n\n')
+actors = g.V(movie.id).bothE('acted_in').bothV().hasLabel(
+    'person').values('name').toList()
+pp.pprint(actors)
+
+print('\nCREW - JOB\n\n')
+crew = g.V(movie.id).bothE('crew_in').toList()
+for member in crew:
+    # Get attribute of edge
+    job = g.E(member.id["@value"]["relationId"]).values('job').next()
+    # Get attribute of actor vertex
+    name = g.E(member.id["@value"]["relationId"]
+               ).bothV().hasLabel('person').values('name').next()
+    print(f'{name} - {job}')
